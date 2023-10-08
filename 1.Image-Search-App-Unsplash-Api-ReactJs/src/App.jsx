a201b66e194d692faf1b44eadcc2baf8920da6ae
mhamza-ali <hamza4841333@gmail.com>
2023-10-08 16:13:10 +0500

    almost done with fteching images

--- 1.Image-Search-App-Unsplash-Api-ReactJs/src/App.jsx
+++ 1.Image-Search-App-Unsplash-Api-ReactJs/src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios'
+import {Form} from 'react-bootstrap'
+import './index.css'
+import { useEffect, useRef, useState } from 'react'
+
+const Access_key = "0hnAtzQj1xfjmeNtflrXB5Aq0ZNtJ-tUB_AlFx2TaBQ"
+const API_URL = "https://api.unsplash.com/search/photos"
+const IMAGES_PER_PAGE = 20;
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const searchInput = useRef(null);
+  const [images, setImages] = useState([])
+  const [totalPages, setTotalPages]  = useState(0)
+  const fetchImages = async () => {
+    try {
+      const {data} = await axios.get(`${API_URL}?query=${searchInput.current.value}&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${Access_key}`)
+      setImages(data.results)
+      setTotalPages(data.total_pages)
+    }
+    
+    catch (error) {
+      console.log(error)
+    }
+  };
+
+  const handleSearch = (event) =>{
+    event.preventDefault()
+    console.log(searchInput.current.value)
+    fetchImages()
+  }
+
+
+  const handleSelection = (selection)=>{
+    searchInput.current.value = selection
+    fetchImages()
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <h1 className='title'>Image Search App</h1>
+      <div className='search-input'>
+      <Form onSubmit={handleSearch}>
+        <Form.Control className='search-input' ref={searchInput} type="type" placeholder="Type something to search" />
+      </Form>  
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='filters'>
+      <div onClick={()=> handleSelection('nature')}>nature</div>
+      <div onClick={()=> handleSelection('birds')}>Birds</div>
+      <div onClick={()=> handleSelection('cars')}>Cars</div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div className='images'>
+            {images.map((image) => (
+              <img
+                key={image.id}
+                src={image.urls.small}
+                alt={image.alt_description}
+                className='image'
+              />
+            ))}
+          </div>
+
+    </div>
+
   )
 }
 
